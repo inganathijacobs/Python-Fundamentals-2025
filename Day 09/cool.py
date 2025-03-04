9e22a1fd0176deaf62b037758167f6a80f1c8f78
Your Name <your.email@example.com>
2025-03-04 08:47:43 +0200

    Day 9 changes

---
+++ Day 09/cool.py
@@ -0,0 +1,12 @@
+def to_upper_case(text):
+    return text.upper() + " 👋"
+
+
+def main():
+    print(__name__)  ##dunder variables
+    print(to_upper_case("Jamie"))
+
+
+if __name__ == "__main__":
+    main()
+# Only when cool.py execute-> Jamie
---
+++ Day 09/fun.py
@@ -0,0 +1,19 @@
+from cool import to_upper_case
+
+print(to_upper_case("Siya"))
+print(__name__)
+
+
+def powerful(n):
+    """squares number
+
+    Args:
+        n (int): number that will be repeated by itself
+
+    Returns:
+        int: number that is squared
+    """
+    return n * n
+
+
+print(powerful(8))
